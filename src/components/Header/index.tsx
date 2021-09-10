839b7491c37a9e92ad2070a6b4122c2d906a27b7
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2021-09-09 22:07:58 -0300

    Subscribe button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,11 @@
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
-import Image from 'next/image'
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Image src="/images/logo.svg" alt="ig.news" width="110" height="31" />
+        <img src="/images/logo.svg" alt="ig.news" />
         <nav>
           <a href="" className={styles.active}>Home</a>
           <a href="">Posts</a>
