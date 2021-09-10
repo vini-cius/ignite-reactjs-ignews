5e9f577880587252d47b1fcc64277b70853ec2c9
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2021-09-10 00:18:30 -0300

    Autenticação com next auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,21 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
+  console.log(session)
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Vinicius Santos
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
 
     </button >
@@ -20,6 +23,7 @@ export function SignInButton() {
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign In with Github
