57e55574b0f12cb75823cfd15c96929a636cd1ff
Vinicius de Souza Santos <vinicius.santos@aztronic.com.br>
2021-09-13 22:33:54 -0300

    Webhooks: ouvindo eventos do Stripe

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Readable } from 'stream';
+import Stripe from 'stripe';
+import { stripe } from '../../services/stripe';
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+    );
+  }
+
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const relevantEvents = new Set([
+  'checkout.session.completed',
+]);
+
+export default async function Webhooks(request: NextApiRequest, response: NextApiResponse) {
+  if (request.method === 'POST') {
+    const buf = await buffer(request);
+    const secret = request.headers['stripe-signature'];
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
+    } catch (err) {
+      return response.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    const type = event.type;
+
+    if (relevantEvents.has(type)) {
+      console.log(`Received event.`, event);
+    }
+
+    response.json({ received: true });
+  } else {
+    response.setHeader("Allow", "POST");
+    response.status(405).end("Method Not Allowed");
+  }
+}
